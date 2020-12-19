91e64d84e0d26881f2410dde963bc8a8dc3d931b
Isaiah Anthony <isaiah.anthony99@gmail.com>
2020-12-19 00:17:59 -0800

    the quiz did work, finishing app broke it, im lost

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ var ans3 = document.querySelector('#answer3')
 var ans4 = document.querySelector('#answer4')
 let timeRemaining = 60
 let score = 0
-let input = document.getElementById('input')
+
 // question time
 let questions = [
   {
@@ -52,21 +52,21 @@ let questions = [
 
 // big functions
 startBtn.addEventListener("click", function () {
-  questionDiv.textContent = `Which index would you choose in order to get cup out of this array ?
+  questionDiv.value = `Which index would you choose in order to get cup out of this array ?
     [water, dog, house, cup, mouse, box, backpack] ?`
-  ans1.textContent = '4'
-  ans2.textContent = '2'
-  ans3.textContent = '3'
-  ans4.textContent = '5'
+  ans1.value = '4'
+  ans2.value = '2'
+  ans3.value = '3'
+  ans4.value = '5'
 
   if (interval === 0) {
     interval = setInterval(function () {
       timeRemaining--;
-      currentTime.textContent = timeRemaining;
+      currentTime.value = timeRemaining;
 
       if (timeRemaining <= 0) {
         clearInterval(interval);
-        currentTime.textContent = "Times Up!"
+        currentTime.value = "Times Up!"
         clearBoard()
       }
     }, 1000);
@@ -77,7 +77,7 @@ startBtn.addEventListener("click", function () {
 
 document.getElementById('next').addEventListener("click", function () {
 
-  if (input.textContent === 'C' || 'c') {
+  if (document.getElementById('input').value === 'C'||'c') {
     score++
     alert('Correct')
     console.log(score);
@@ -87,7 +87,7 @@ document.getElementById('next').addEventListener("click", function () {
     alert('Wrong');
     console.log(score);
   }
-  input.textContent = ''
+  document.getElementById('input').value = ''
   question2()
   document.getElementById('next').style.display = 'none'
   document.getElementById('next2').style.display = 'block'
@@ -96,7 +96,7 @@ document.getElementById('next').addEventListener("click", function () {
 
 document.getElementById('next2').addEventListener("click", function () {
 
-  if (input.textContent === 'B' || 'b') {
+  if (document.getElementById('input').value === 'B' || 'b') {
     score++
     alert('Correct')
     console.log(score);
@@ -106,7 +106,7 @@ document.getElementById('next2').addEventListener("click", function () {
     alert('Wrong');
     console.log(score);
   }
-  input.textContent = ''
+  document.getElementById('input').value = ''
   question3()
   document.getElementById('next2').style.display = 'none'
   document.getElementById('next3').style.display = 'block'
@@ -114,7 +114,7 @@ document.getElementById('next2').addEventListener("click", function () {
 
 document.getElementById('next3').addEventListener("click", function () {
 
-  if (input.textContent === 'C' || 'c') {
+  if (document.getElementById('input').value === 'C' || 'c') {
     score++
     alert('Correct')
     console.log(score);
@@ -124,7 +124,7 @@ document.getElementById('next3').addEventListener("click", function () {
     alert('Wrong');
     console.log(score);
   }
-  input.textContent = ''
+  document.getElementById('input').value = ''
   question4()
   document.getElementById('next3').style.display = 'none'
   document.getElementById('next4').style.display = 'block'
@@ -132,7 +132,7 @@ document.getElementById('next3').addEventListener("click", function () {
 
 document.getElementById('next4').addEventListener("click", function () {
 
-  if (input.textContent === 'A' || 'a') {
+  if (document.getElementById('input').value === 'A' || 'a') {
     score++
     alert('Correct')
     console.log(score);
@@ -142,7 +142,7 @@ document.getElementById('next4').addEventListener("click", function () {
     alert('Wrong');
     console.log(score);
   }
-  input.textContent = ''
+  document.getElementById('input').value = ''
   question5()
   document.getElementById('next4').style.display = 'none'
   document.getElementById('submit').style.display = 'block'
@@ -150,7 +150,7 @@ document.getElementById('next4').addEventListener("click", function () {
 
 document.getElementById('submit').addEventListener("click", function () {
 
-  if (input.textContent === 'C' || 'c') {
+  if (document.getElementById('input').value === 'A' || 'a') {
     score++
     alert('Correct')
     console.log(score);
@@ -160,7 +160,7 @@ document.getElementById('submit').addEventListener("click", function () {
     alert('Wrong');
     console.log(score);
   }
-  input.textContent = ''
+  document.getElementById('input').value = ''
   clearBoard()
   document.getElementById('submit').style.display = 'none'
   document.getElementById('initial').style.display = 'block'
@@ -181,56 +181,56 @@ document.getElementById('initial').addEventListener("click", function record() {
 
 // functions to be called
 function question2() {
-  questionDiv.textContent = 'Which symbol is used to call elements in CSS?'
-  ans1.textContent = '!'
-  ans2.textContent = '#'
-  ans3.textContent = '.'
-  ans4.textContent = '@'
+  questionDiv.value = 'Which symbol is used to call elements in CSS?'
+  ans1.value = '!'
+  ans2.value = '#'
+  ans3.value = '.'
+  ans4.value = '@'
 
 }
 
 function question3() {
-  questionDiv.textContent = 'What is the shorthand to increase an increment by 1?'
-  ans1.textContent = '--'
-  ans2.textContent = '..'
-  ans3.textContent = '++'
-  ans4.textContent = '**'
+  questionDiv.value = 'What is the shorthand to increase an increment by 1?'
+  ans1.value = '--'
+  ans2.value = '..'
+  ans3.value = '++'
+  ans4.value = '**'
 }
 
 function question4() {
-  questionDiv.textContent = 'When do you need to add the event.preventDefault function?'
-  ans1.textContent = 'When a button is inside a form.'
-  ans2.textContent = 'When embedding a video or image into the HTML file.'
-  ans3.textContent = 'Immediately upon loading the web page.'
-  ans4.textContent = 'Anytime the tag <span> exists.'
+  questionDiv.value = 'When do you need to add the event.preventDefault function?'
+  ans1.value = 'When a button is inside a form.'
+  ans2.value = 'When embedding a video or image into the HTML file.'
+  ans3.value = 'Immediately upon loading the web page.'
+  ans4.value = 'Anytime the tag <span> exists.'
 }
 
 function question5() {
-  questionDiv.textContent = 'What is rubber ducking?'
-  ans1.textContent = 'A method of debugging.'
-  ans2.textContent = 'A type of server lag.'
-  ans3.textContent = 'A common office game played among coworkers.'
-  ans4.textContent = 'One of many error code messages.'
+  questionDiv.value = 'What is rubber ducking?'
+  ans1.value = 'A method of debugging.'
+  ans2.value = 'A type of server lag.'
+  ans3.value = 'A common office game played among coworkers.'
+  ans4.value = 'One of many error code messages.'
 }
 
 function clearBoard() {
-  questionDiv.textContent = 'Quiz complete.'
-  ans1.textContent = 'Your Score ' + score
-  ans2.textContent = ''
-  ans3.textContent = ''
-  ans4.textContent = ''
-  document.getElementById('a').textContent = ''
-  document.getElementById('b').textContent = ''
-  document.getElementById('c').textContent = ''
-  document.getElementById('d').textContent = ''
+  questionDiv.value = 'Quiz complete.'
+  ans1.value = 'Your Score ' + score
+  ans2.value = ''
+  ans3.value = ''
+  ans4.value = ''
+  document.getElementById('a').value = ''
+  document.getElementById('b').value = ''
+  document.getElementById('c').value = ''
+  document.getElementById('d').value = ''
 }
 
 function reset() {
   score = 0
   timeRemaining = 60
-  questionDiv.textContent = 'Example text'
-  ans1.textContent = 'example text'
-  ans2.textContent = 'example text'
-  ans3.textContent = 'example text'
-  ans4.textContent = 'example text'
+  questionDiv.value = 'Example text'
+  ans1.value = 'example text'
+  ans2.value = 'example text'
+  ans3.value = 'example text'
+  ans4.value = 'example text'
 }
