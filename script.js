1a278ff45e3940f25f7cdcf17718adcfdd766b5d
Isaiah Anthony <isaiah.anthony99@gmail.com>
2020-12-18 21:47:09 -0800

    main quiz finished

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // assorted variables
-var p = document.getElementById('nextContainer');
 var questionIndex = 0
 var startBtn = document.querySelector('#start')
 var questionDiv = document.querySelector('#currentQuestion')
@@ -16,23 +15,23 @@ let score = 0
 let questions = [
   {
     question: `Which index would you choose in order to get cup out of this array?
-  [water, dog, house, cup, mouse, box, backpack]?`,
+    [water, dog, house, cup, mouse, box, backpack]?`,
     correctAnswer: 'C',
     wrongAnswers: 'A, B, D',
   },
-
+  
   {
     question: 'Which symbol is used to call elements in CSS?',
     correctAnswer: 'B',
     wrongAnswers: 'A, C, D',
   },
-
+  
   {
     question: 'What is the shorthand to increase an increment by 1?',
     correctAnswer: 'C',
     wrongAnswers: 'A, B, D',
   },
-
+  
   {
     question: 'When do you need to add the event.preventDefault function?',
     correctAnswer: 'A',
@@ -53,26 +52,26 @@ let questions = [
 // big functions
 startBtn.addEventListener("click", function () {
   questionDiv.textContent = `Which index would you choose in order to get cup out of this array ?
-    [water, dog, house, cup, mouse, box, backpack] ?`
+  [water, dog, house, cup, mouse, box, backpack] ?`
   ans1.textContent = '4'
   ans2.textContent = '2'
   ans3.textContent = '3'
   ans4.textContent = '5'
-
+  
   if (interval === 0) {
     interval = setInterval(function () {
       timeRemaining--;
       currentTime.textContent = timeRemaining;
-
+      
       if (timeRemaining <= 0) {
         clearInterval(interval);
         currentTime.textContent = "Times Up!"
         clearBoard()
       }
     }, 1000);
-
+    
   }
-
+  
 })
 
 document.getElementById('next').addEventListener("click", function () {
@@ -84,14 +83,86 @@ document.getElementById('next').addEventListener("click", function () {
   }
   else {
     timeRemaining = timeRemaining - 10
-      alert('Wrong');
-      console.log(score);
-    }
+    alert('Wrong');
+    console.log(score);
+  }
   document.getElementById('input').value = ''
   question2()
   removeElement1()
   addElement2()
-
+  
+})
+document.getElementById('next2').addEventListener("click", function () {
+  
+  if (document.getElementById('input').value === 'B'||'b') {
+    score++
+    alert('Correct')
+    console.log(score);
+  }
+  else {
+    timeRemaining = timeRemaining - 10
+    alert('Wrong');
+    console.log(score);
+  }
+  document.getElementById('input').value = ''
+  question3()
+  removeElement2()
+  addElement3()
+  
+})
+document.getElementById('next3').addEventListener("click", function () {
+  
+  if (document.getElementById('input').value === 'C'||'c') {
+    score++
+    alert('Correct')
+    console.log(score);
+  }
+  else {
+    timeRemaining = timeRemaining - 10
+    alert('Wrong');
+    console.log(score);
+  }
+  document.getElementById('input').value = ''
+  question4()
+  removeElement3()
+  addElement4()
+  
+})
+document.getElementById('next4').addEventListener("click", function () {
+  
+  if (document.getElementById('input').value === 'A'||'a') {
+    score++
+    alert('Correct')
+    console.log(score);
+  }
+  else {
+    timeRemaining = timeRemaining - 10
+    alert('Wrong');
+    console.log(score);
+  }
+  document.getElementById('input').value = ''
+  question5()
+  removeElement4()
+  addElement5()
+  
+})
+document.getElementById('submit').addEventListener("click", function () {
+  
+  if (document.getElementById('input').value === 'C'||'c') {
+    score++
+    alert('Correct')
+    console.log(score);
+  }
+  else {
+    timeRemaining = timeRemaining - 10
+    alert('Wrong');
+    console.log(score);
+  }
+  document.getElementById('input').value = ''
+  clearBoard()
+  removeElement5()
+  addElement1()
+  
 })
 
 
@@ -142,19 +213,73 @@ function clearBoard() {
   document.getElementById('d').textContent = ''
 }
 
-function removeElement1(next) {
+function removeElement1() {
   
-  var element = document.getElementById('next');
-  element.remove('next')
+  document.getElementById('next').remove()
+  console.log('hello');
+}
+function removeElement2() {
+  
+  document.getElementById('next2').remove()
+  console.log('hello');
+}
+function removeElement3() {
+  
+  document.getElementById('next3').remove()
+  console.log('hello');
+}
+function removeElement4() {
+  
+  document.getElementById('next4').remove()
   console.log('hello');
 }
 
 function addElement2() {
-
+  
   var newElement = document.createElement('button');
   newElement.setAttribute('id', 'next2');
-  newElement.innerHTML = '<button type= "button" class = "btn btn-primary">Next Question</button>';
-  p.appendChild(newElement);
- 
+  newElement.setAttribute('type', 'button');
+  newElement.setAttribute('class', 'btn btn-primary');
+  newElement.innerHTML = 'Next Question';
+  document.getElementById('nextContainer').appendChild(newElement)
 }
+function addElement3() {
+  
+  var newElement = document.createElement('button');
+  newElement.setAttribute('id', 'next3');
+  newElement.setAttribute('type', 'button');
+  newElement.setAttribute('class', 'btn btn-primary');
+  newElement.innerHTML = 'Next Question';
+  document.getElementById('nextContainer').appendChild(newElement)
+}
+function addElement4() {
+  
+  var newElement = document.createElement('button');
+  newElement.setAttribute('id', 'next4');
+  newElement.setAttribute('type', 'button');
+  newElement.setAttribute('class', 'btn btn-primary');
+  newElement.innerHTML = 'Next Question';
+  document.getElementById('nextContainer').appendChild(newElement)
+}
+function addElement5() {
+  
+  var newElement = document.createElement('button');
+  newElement.setAttribute('id', 'Submit');
+  newElement.setAttribute('type', 'button');
+  newElement.setAttribute('class', 'btn btn-primary');
+  newElement.innerHTML = 'Submit Answers';
+  document.getElementById('nextContainer').appendChild(newElement)
+}
+function addElement6() {
+  
+  var newElement = document.createElement('button');
+  newElement.setAttribute('id', 'intial');
+  newElement.setAttribute('type', 'button');
+  newElement.setAttribute('class', 'btn btn-primary');
+  newElement.innerHTML = 'Record Score';
+  document.getElementById('nextContainer').appendChild(newElement)
+}
+  
+  
+  
 
